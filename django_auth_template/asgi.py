0d81b7e83ee2244ab5dfbbd6b13c39bc83e86508
Compenso <wesurbaniakmusic@gmail.com>
2020-08-05 09:02:22 -0600

    Initial commit
    .env file created
    filled with ENV, DATABASE Name, SECRET
    All install and init is done.

---
+++ django_auth_template/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Little_Life_Goals project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Little_Life_Goals.settings')
+
+application = get_asgi_application()
